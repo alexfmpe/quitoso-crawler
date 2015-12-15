8bab9c8f76ce6d4e0373a0d70cf9b2b716b2f9ec
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2015-12-15 16:55:40 +0000

    group parsing

--- xhr.js
+++ xhr.js
@@ -1,5 +1,11 @@
 var jsdom = require("jsdom")
+
+var root  = "http://www.dgv.min-agricultura.pt/portal/page/portal/DGV/genericos?generico=4183425&cboui=4183425"
+var group = "http://www.dgav.pt/fitofarmaceuticos/guia/Introd_guia/herbicidas_guia.htm"
 var url = "http://www.dgav.pt/fitofarmaceuticos/guia/finalidades_guia/Herbicidas/tomateiro1.htm"
+var scripts = ["http://code.jquery.com/jquery.js"]
+var config = { encoding: "binary" }
+
 var log = console.log
 var ERROR = {
   multipleTables : "GTFO: Found more than one <table> on page"
@@ -7,10 +13,13 @@ var ERROR = {
 
 main()
 
+
+function fetch(url, callback) {
+    jsdom.env(url, scripts, config, callback)
+}
+
 function main() {
-  var scripts = ["http://code.jquery.com/jquery.js"]
-  var config = { encoding: "binary" }
-  jsdom.env(url, scripts, config, parseTable);
+  fetch(group, parseGroup)
 }
 
 function GTFO(e) {
@@ -18,12 +27,32 @@ function GTFO(e) {
   process.exit(1)
 }
 
-function parseTable(err, window) {
+function relativeURL(origin, path) {
+  var parent = origin.match((/(.*\/)+/))[0]
+  return parent + path
+}
+
+function parseGroup(err, window) {
+  window.$("tr").each(parseRow)
+
+  function parseRow(i, tr) {
+    var rows = window.$("td", tr)
+    var [horticulture, date] = rows.toArray()
+    if(! /\w+/.test(horticulture.textContent)) return;
+
+    var href = window.$('a', horticulture).attr("href")
+    var url = relativeURL(window.location.href, href)
+	log(url)
+    fetch(url, parseHorticulture)
+  }
+}
+
+function parseHorticulture(err, window) {
   var $ = window.$
   var _infestant = ""
   var _substance = ""
 
-  if($("table").length > 1)   GTFO(ERROR.multipleTables);
+  if($("table").length > 1)   GTFO(window.location.href + "\t" + ERROR.multipleTables);
   $("tr").each(parseRow)
 
   function parseRow(i, tr) {
@@ -31,16 +60,8 @@ function parseTable(err, window) {
     var [infestant, substance, formulation, dosage, days, notes] = cells.toArray()
     _infestant = infestant.textContent.trim() == "" ? _infestant : infestant
     _substance = substance.textContent.trim() == "" ? _substance : substance
-    var entry = new Entry(_infestant, _substance, formulation, dosage, days, notes)
-    log(entry.notes.textContent)
+    log(_substance.textContent)
   }
 }
 
-function Entry(infestant, substance, formulation, dosage, days, notes) {
-  this.infestant = infestant
-  this.substance = substance
-  this.formulation = formulation
-  this.dosage = dosage
-  this.days = days
-  this.notes = notes
-}
+
